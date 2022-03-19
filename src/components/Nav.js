8bc0086ad1640f46bc088342d89e5812e8cdc6ef
yeeun_An <an446@naver.com>
2022-03-19 17:42:35 +0900

    :sparkles: nav - bg scroll event

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "./Nav.css";
+
 export default function Nav() {
+  const [show, setshow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if(window.scrollY > 50){
+        setshow(true);
+      }else{
+        setshow(false);
+      }
+    })
+  
+    return () => {
+      window.removeEventListener("scroll",() => {});
+    }
+  }, [])
+  
   return (
-    <nav>
+    // show가 true일때는 nav__black 클래스 넣어주기
+    /*
+    1. <조건문> || <조건문이 거짓일 때 실행될 코드>
+    2. <조건문> && <조건문이 참일 때 실행될 코드>
+    */
+    <nav className={`nav ${show && "nav__black"}`}>
       <img alt='logo'
-      src='../img/logo.png'
+      src='https://user-images.githubusercontent.com/48678872/159113400-7f9ba21f-c25d-4194-a777-223cad613bc4.png'
       className='nav__logo'
       onClick={() => window.location.reload()}
       >
@@ -12,7 +34,7 @@ export default function Nav() {
 
       <img
       alt="user avatar"
-      src="../img/default_avatar.png"
+      src="https://user-images.githubusercontent.com/48678872/159111910-222a3b1e-5e45-4b81-9a29-0b205e37569e.png"
       className='nav__avatar'
       >
       </img>
