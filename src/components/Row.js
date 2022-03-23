7bb0dae92ff2a42bd25df39070d443befff533a5
yeeun_An <an446@naver.com>
2022-03-23 19:56:14 +0900

    :sparkle: slide control 기능

--- src/components/Row.js
+++ src/components/Row.js
@@ -13,13 +13,16 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
     //영화 정보들 movies에 넣어주기
     setMovies(request.data.results);
   };
-
+  console.log(id)
   return (
     <section className="row">
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow">{"<"}</span>
+          <span className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}>{"<"}</span>
         </div>
         <div id={id} className="row__posters">
           {movies.map((movies) => (
@@ -36,7 +39,10 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
           ))}
         </div>
         <div className="slider__arrow-right">
-          <span className="arrow">{">"}</span>
+          <span className="arrow"
+          onClick={() => {
+            document.getElementById(id).scrollLeft += window.innerWidth - 80;
+          }}>{">"}</span>
         </div>
       </div>
     </section>
