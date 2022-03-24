e2edf3da7971d618082ec65a07e4ce8b537eeeab
yeeun_An <an446@naver.com>
2022-03-24 14:54:15 +0900

    :sparkles: Row에서 영화 클릭 시, 모달창 여는 기능 - 모달에 movies 정보 전달

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,14 @@
 import axios from "../api/axios";
 import React, { useEffect, useState } from "react";
 import "./Row.css";
+import { MovieModal } from "./MovieModal/index";
 
 export default function Row({ isLargeRow, title, id, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  // 클릭시 모달에 무비 정보 보내기 위해 생성한 스테이트
+  const [movieSelected, setMovieSelected] = useState([]);
+
   useEffect(() => {
     fetchMovieData();
   }, []);
@@ -13,16 +18,26 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
     //영화 정보들 movies에 넣어주기
     setMovies(request.data.results);
   };
-  console.log(id)
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    //클릭하면 셋무비셀렉티드 안에 해당 영화정보를 전달
+    setMovieSelected(movies);
+  };
+  // console.log(id)
   return (
     <section className="row">
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow"
+          <span
+            className="arrow"
             onClick={() => {
               document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-            }}>{"<"}</span>
+            }}
+          >
+            {"<"}
+          </span>
         </div>
         <div id={id} className="row__posters">
           {movies.map((movies) => (
@@ -34,17 +49,28 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
               src={`https://image.tmdb.org/t/p/original/${
                 isLargeRow ? movies.poster_path : movies.backdrop_path
               }`}
+              // 영화클릭시 모달창 띄우기
+              onClick={() => handleClick(movies)}
               alt={movies.name}
             />
           ))}
         </div>
         <div className="slider__arrow-right">
-          <span className="arrow"
-          onClick={() => {
-            document.getElementById(id).scrollLeft += window.innerWidth - 80;
-          }}>{">"}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
         </div>
       </div>
+
+      {/* ModalOpen이 true일 경우 */}
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
     </section>
   );
 }
