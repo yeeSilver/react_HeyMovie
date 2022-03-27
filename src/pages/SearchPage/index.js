bdb7198eae237e2e69a6bee6a6e3dc93121ced6e
yeeun_An <an446@naver.com>
2022-03-27 16:11:24 +0900

    zap: 클릭시 영화 상세 페이지 useParams

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,15 +1,16 @@
 import axios from "../../api/axios";
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import {useDebounce} from "../../hooks/useDebounce";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useDebounce } from "../../hooks/useDebounce";
 import "./SearchPage.css";
 
 export default function SearchPage() {
+  const navigate = useNavigate();
+  const [searchResults, setSearchResults] = useState([]);
   const useQuery =() =>{
     // useLocation에는 search한 url정보들이 들어있음.
     return new URLSearchParams(useLocation().search);
-  }
-  const [searchResults, setSearchResults] = useState([]);
+  };
 
   let query = useQuery();
   // 상단 url중에 q=(여기에 들어가는 것이 searchTerm)
@@ -17,12 +18,14 @@ export default function SearchPage() {
   const debouncedSearchTerm = useDebounce(searchTerm, 5000);
 
   useEffect(() => {
-    if(debouncedSearchTerm){
+    if (debouncedSearchTerm) {
       fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [debouncedSearchTerm]); //debouncedSearchTerm이 변할때마다 fetchSearchMovie 함수 콜함
+  }, [debouncedSearchTerm]);
+  //debouncedSearchTerm이 변할때마다 fetchSearchMovie 함수 콜함
   
   const fetchSearchMovie = async (searchTerm) => {
+    console.log("searchTerm", searchTerm);
     try {
       const request = await axios.get(
         `/search/multi?include_adult=false&query=${searchTerm}`
@@ -41,25 +44,27 @@ export default function SearchPage() {
           // (데이터가 있는 경우) backdrop_path 이미지가 있고 person 타입이 아닌 경우
           if(movie.backdrop_path !== null && movie.media_type !== "person"){
             // wide500사이즈
-            const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
-            return(
+            const movieImageUrl =
+            "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+          return (
               <div className="movie" key={movie.id}>
-                <div className="movie__column-poster">
-                <img className="movie__poster" src={movieImageUrl} alt="movie image"/>
+                {/* :movie.id로 페이지 이동 -> app.js에 선언한 바에 따라 detail페이지로 이동함 */}
+                <div className="movie__column-poster"  onClick={() => navigate(`/${movie.id}`)}>
+                <img className="movie__poster" src={movieImageUrl} alt="movie poster"/>
                 </div>
               </div>
-            )
+            );
           }
         })}
         </section>
         // (데이터 없는 경우)
-      ) : (<section className="no-results">
+      ) : (
+      <section className="no-results">
         <div className="no-results__text">
           <p>
             {debouncedSearchTerm}에 맞는 결과가 없습니다.
           </p>
         </div>
-
       </section>
     ))}
 
